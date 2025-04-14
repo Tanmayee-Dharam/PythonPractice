f26dc12f97576ec700531de270d74331533e181d
Tanmayee-Dharam <tanmayee.dharam@gmail.com>
2025-04-13 19:38:15 -0500

    added number pattern reverse

--- number-pattern.py
+++ number-pattern.py
@@ -1,4 +1,4 @@
-print("Number Pattern ")
+print("Number Pattern 1 ")
 
 # Decide the row count. (above pattern contains 5 rows)
 row = 5
@@ -11,4 +11,14 @@ for i in range(1, row + 1, 1):
     for j in range(1, i + 1):
         print(j, end=' ')
     # empty line after each row
+    print("")
+
+print()  #adding a blank line between patterns
+
+print("Number Pattern 2")
+n = 5
+k = 5
+for i in range(0,n+1):
+    for j in range(k-i,0,-1):
+        print(j,end=' ')
     print("")
