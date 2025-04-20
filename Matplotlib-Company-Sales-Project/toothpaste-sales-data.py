e9c7b1ce93f3f35c6a1ae2c79c6a035f874df645
Tanmayee-Dharam <tanmayee.dharam@gmail.com>
2025-04-19 23:56:11 -0500

    code for scatter plot

---
+++ Matplotlib-Company-Sales-Project/toothpaste-sales-data.py
@@ -0,0 +1,19 @@
+"""
+Question 4: Read toothpaste sales data of each month and show it using a scatter plot
+Also, add a grid in the plot. grid line style should “–“.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/Tanmayee Dharam/OneDrive - The University of Texas at Dallas/Desktop/company_sales_data.csv")
+monthList  = df ['month_number'].tolist()
+toothPasteSalesData = df ['toothpaste'].tolist()
+plt.scatter(monthList, toothPasteSalesData, label = 'Toothpaste Sales data')
+plt.xlabel('Month Number')
+plt.ylabel('Number of units Sold')
+plt.legend(loc='upper left')
+plt.title(' Tooth paste Sales data')
+plt.xticks(monthList)
+plt.grid(True, linewidth= 1, linestyle="--")
+plt.show()
